53adb9fc8749bdb0383791688cc2b1001492e604
Scylla Diakite <scylladiakite@Scyllas-MacBook-Pro.local>
2025-12-19 17:45:35 +0000

    fix messages display order

--- src/services/feedbackService.ts
+++ src/services/feedbackService.ts
@@ -129,7 +129,7 @@ export const feedbackService = {
         let q = query(
             feedbacksRef,
             where('phaseId', '==', phaseId),
-            orderBy('createdAt', 'desc')
+            orderBy('createdAt', 'asc')
         );
 
         if (stepId) {
